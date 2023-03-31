141f59fe6bfa78acd53132a10621bc067f5d3348
Vinícius Rampelotti <100747383+vini-rampelotti@users.noreply.github.com>
2023-03-30 21:22:11 -0300

    Add files via upload

---
+++ ExerciciosAula/ExerciciosAula/src/Calculadora/testeCalculadora.java
@@ -0,0 +1,18 @@
+package Calculadora;
+import java.util.Scanner;
+
+public class testeCalculadora {
+
+	public static void main(String[] args) {
+		calcula c = new calcula();
+		int opcao = 1;
+		int operando1 = 10;
+		int operando2 = 20;
+		int resultado;
+		float operandof1 = 10.99;
+		float operandof2 = 20.99;
+		
+
+	}
+
+}
